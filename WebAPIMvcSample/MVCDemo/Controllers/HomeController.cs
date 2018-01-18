c5531398034d3f2584796e4e9ce8d0a4c14a3573
srikanthpitta <srikanth42.p@gmail.com>
2018-01-18 17:37:44 +0530

    First Branch is Created

--- WebAPIMvcSample/MVCDemo/Controllers/HomeController.cs
+++ WebAPIMvcSample/MVCDemo/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace MVCDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "This is the Index action method";
         }
 
         public string About()
